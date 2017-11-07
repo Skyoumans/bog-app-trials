f12670bbc7f727a418c3da02c4fc8a1991cfae2c
Skyoumans <skyyoumans@gmail.com>
2017-11-07 10:50:30 -0500

    initial commit Rails routes and controllers fleshed out

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Creature.destroy_all
+
+Creature.create({name: "Luke", description: "Jedi, son of Vader"})
+Creature.create({name: "Darth Vader", description: "Sith Lord, father of Luke and Leia"})
+Creature.create({name: "Yoda", description: "Little green alien and Master Jedi"})
+Creature.create({name: "Leia", description: "Twin sister of Luke, daughter of Vader"})
